fc970c95080259f45a7011e0ea1415e3c7e4ea3d
jack-skelton <jack.s1995@live.com.au>
2017-01-31 04:28:42 +1100

    functionality complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var mysql = require('mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var database = {
+  host     : 'localhost',
+  user     : 'root',
+  password : 'skelton1',
+  database : 'publicchat'
+}
+
+
+/* POST add message */
+router.post('/', function(req, res, next) {
+  if (req.body.message || req.body.name) {
+    req.body.message = req.body.message || 'nothing';
+    req.body.name = req.body.name || 'anonymous';
+
+    var connection = mysql.createConnection(database);
+    connection.query('delete from messages limit 1', function (error, results, fields) {if (error) throw error;});
+    connection.query('insert into messages values(' + mysql.escape(req.body.name) + ',' + mysql.escape(req.body.message) + "," +  mysql.escape(moment().format('DD-MM hh:mm:ss')) + ");", 
+    function (error, results, fields) {
+      if (error) throw error;
+      connection.end();
+      next();
+    });
+  }
+});
+
+router.all('/', function(req, res, next) {
+  var connection = mysql.createConnection(database);
+  connection.query('select * from messages', 
+  function (error, results, fields) {
+    if (error) throw error;
+    var messages = results;
+
+    res.render('index', {messages});
+  });
+
+
 });
 
 module.exports = router;
